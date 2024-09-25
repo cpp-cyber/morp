669ba6af9ce9375c4fdb6a8fb8e17ea78089658f
meeks <maxwell.caron1@gmail.com>
2024-09-25 02:06:45 -0700

    Updated getTodoResponse

--- handlers.go
+++ handlers.go
@@ -14,100 +14,101 @@ func PingHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 }
 
 func TodoHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    if len(i.ApplicationCommandData().Options) == 0 {
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "Please provide a subcommand",
-            },
-        })
-        return
-    }
-    switch i.ApplicationCommandData().Options[0].Name {
-    case "add":
-        if len(i.ApplicationCommandData().Options[0].Options) < 2 {
-            s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-                Type: discordgo.InteractionResponseChannelMessageWithSource,
-                Data: &discordgo.InteractionResponseData{
-                    Content: "Please provide a person and a task",
-                },
-            })
-            return
-        }
-        user := i.ApplicationCommandData().Options[0].Options[0].StringValue()
-        content := i.ApplicationCommandData().Options[0].Options[1].StringValue()
-        err := addTodo(user, content)
-        addTodoResponse(s, i, err)
-    case "get":
-        if len(i.ApplicationCommandData().Options[0].Options) == 0 {
-            allTodos, err := getAllTodos()
-            if err != nil {
-                s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-                    Type: discordgo.InteractionResponseChannelMessageWithSource,
-                    Data: &discordgo.InteractionResponseData{
-                        Content: "Failed to get todos: " + err.Error(),
-                    },
-                })
-                return
-            }
-            getAllTodoResponse(s, i, allTodos, err)
-            return
-        }
-        user := i.ApplicationCommandData().Options[0].Options[0].StringValue()
-        todos, err := getTodos(user)
-        getTodoResponse(s, i, todos, err)
-    case "complete":
-        if len(i.ApplicationCommandData().Options[0].Options) == 0 {
-            s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-                Type: discordgo.InteractionResponseChannelMessageWithSource,
-                Data: &discordgo.InteractionResponseData{
-                    Content: "Please provide an id",
-                },
-            })
-            return
-        }
-        id := i.ApplicationCommandData().Options[0].Options[0].IntValue()
-        todo, err := completeTodoById(int(id))
-        completeTodoResponse(s, i, &todo, err)
-    }
+	if len(i.ApplicationCommandData().Options) == 0 {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Please provide a subcommand",
+			},
+		})
+		return
+	}
+	switch i.ApplicationCommandData().Options[0].Name {
+	case "add":
+		if len(i.ApplicationCommandData().Options[0].Options) < 2 {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Please provide a person and a task",
+				},
+			})
+			return
+		}
+		user := i.ApplicationCommandData().Options[0].Options[0].StringValue()
+		content := i.ApplicationCommandData().Options[0].Options[1].StringValue()
+		err := addTodo(user, content)
+		addTodoResponse(s, i, err)
+	case "get":
+		if len(i.ApplicationCommandData().Options[0].Options) == 0 {
+			allTodos, err := getAllTodos()
+			if err != nil {
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "Failed to get todos: " + err.Error(),
+					},
+				})
+				return
+			}
+			getAllTodoResponse(s, i, allTodos, err)
+			return
+		}
+		user := i.ApplicationCommandData().Options[0].Options[0].StringValue()
+		todos, err := getTodos(user)
+		numCompleted, err := getNumCompleted(user)
+		getTodoResponse(s, i, todos, numCompleted, err)
+	case "complete":
+		if len(i.ApplicationCommandData().Options[0].Options) == 0 {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Please provide an id",
+				},
+			})
+			return
+		}
+		id := i.ApplicationCommandData().Options[0].Options[0].IntValue()
+		todo, err := completeTodoById(int(id))
+		completeTodoResponse(s, i, &todo, err)
+	}
 }
 
 func KaminoHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    switch i.ApplicationCommandData().Options[0].Name {
-    case "get-pods":
-        getPods(s, i)
-    case "delete-pod":
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "Deleting pod...",
-            },
-        })
-        deletePod(s, i)
-    case "bulk-delete":
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "Deleting pods...",
-            },
-        })
-        bulkDeletePods(s, i)
-    case "refresh":
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "Refreshing templates...",
-            },
-        })
-        refreshTemplates(s, i)
-    case "competition-clone":
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "Cloning competition...",
-            },
-        })
-        competitionClone(s, i)
-    }
+	switch i.ApplicationCommandData().Options[0].Name {
+	case "get-pods":
+		getPods(s, i)
+	case "delete-pod":
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Deleting pod...",
+			},
+		})
+		deletePod(s, i)
+	case "bulk-delete":
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Deleting pods...",
+			},
+		})
+		bulkDeletePods(s, i)
+	case "refresh":
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Refreshing templates...",
+			},
+		})
+		refreshTemplates(s, i)
+	case "competition-clone":
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Cloning competition...",
+			},
+		})
+		competitionClone(s, i)
+	}
 
 }
--- todo.go
+++ todo.go
@@ -2,114 +2,151 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/Clinet/discordgo-embed"
 	"github.com/bwmarrin/discordgo"
+	"regexp"
 )
 
 type todo struct {
-    Id int `sql:"AUTO_INCREMENT" gorm:"primaryKey"`
-    User string
-    Task string
-    Completed bool
+	Id        int `sql:"AUTO_INCREMENT" gorm:"primaryKey"`
+	User      string
+	Task      string
+	Completed bool
+}
+
+func getDiscordUser(userMention string) (*discordgo.Member, error) {
+	// Extract the user ID from the mention using regex
+	re := regexp.MustCompile(`<@!?(\d+)>`)
+	matches := re.FindStringSubmatch(userMention)
+	if len(matches) < 2 {
+		return nil, fmt.Errorf("no user ID found in mention")
+	}
+	userID := matches[1]
+
+	// Get the user by ID
+	member, err := s.GuildMember(config.DiscordGuildID, userID)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving user from guild: %v", err)
+	}
+
+	return member, nil
 }
 
 func completeTodoResponse(s *discordgo.Session, i *discordgo.InteractionCreate, todo *todo, err error) {
-    if err != nil {
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "Failed to complete todo: " + err.Error(),
-            },
-        })
-        return
-    }
+	if err != nil {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to complete todo: " + err.Error(),
+			},
+		})
+		return
+	}
 
-    embed := embed.NewEmbed()
-    embed.SetTitle("Todo Completed")
-    embed.SetColor(0xffab40)
-    embed.AddField("Person", todo.User)
-    embed.AddField("Task", todo.Task)
+	embed := embed.NewEmbed()
+	embed.SetTitle("Todo Completed")
+	embed.SetColor(0xE0C460)
+	embed.AddField("Person", todo.User)
+	embed.AddField("Task", todo.Task)
 
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Embeds: []*discordgo.MessageEmbed{embed.MessageEmbed},
-        },
-    })
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{embed.MessageEmbed},
+		},
+	})
 }
 
 func addTodoResponse(s *discordgo.Session, i *discordgo.InteractionCreate, err error) {
-    if err != nil {
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "Failed to add todo: " + err.Error(),
-            },
-        })
-        return
-    }
+	if err != nil {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to add todo: " + err.Error(),
+			},
+		})
+		return
+	}
 
-    embed := embed.NewEmbed()
-    embed.SetTitle("Todo Added")
-    embed.SetColor(0xffab40)
-    embed.AddField("Person", i.ApplicationCommandData().Options[0].Options[0].StringValue())
-    embed.AddField("Task", i.ApplicationCommandData().Options[0].Options[1].StringValue())
+	embed := embed.NewEmbed()
+	embed.SetTitle("Todo Added")
+	embed.SetColor(0xE0C460)
+	embed.AddField("Person", i.ApplicationCommandData().Options[0].Options[0].StringValue())
+	embed.AddField("Task", i.ApplicationCommandData().Options[0].Options[1].StringValue())
 
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Embeds: []*discordgo.MessageEmbed{embed.MessageEmbed},
-        },
-    })
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{embed.MessageEmbed},
+		},
+	})
 }
 
-func getTodoResponse(s *discordgo.Session, i *discordgo.InteractionCreate, todos []todo, err error) {
-        if err != nil {
-            s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-                Type: discordgo.InteractionResponseChannelMessageWithSource,
-                Data: &discordgo.InteractionResponseData{
-                    Content: "Failed to get todos: " + err.Error(),
-                },
-            })
-            return
-        }
+func getTodoResponse(s *discordgo.Session, i *discordgo.InteractionCreate, todos []todo, numCompleted int64, err error) {
+	if err != nil {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to get todos: " + err.Error(),
+			},
+		})
+		return
+	}
 
-        embed := embed.NewEmbed()
-        embed.SetTitle("Todos for " + todos[0].User)
-        embed.SetColor(0xffab40)
-        for _, t := range todos {
-            embed.AddField(fmt.Sprintf("Id: %d", t.Id), fmt.Sprintf("Task: %s\n Completed: %t", t.Task, t.Completed))
-        }
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Embeds: []*discordgo.MessageEmbed{embed.MessageEmbed},
-            },
-        })
+	member, err := getDiscordUser(todos[0].User)
+	tasks := []*discordgo.MessageEmbedField{}
+
+	for _, t := range todos {
+		tasks = append(tasks, &discordgo.MessageEmbedField{
+			Name:   fmt.Sprintf("ID: %d", t.Id),
+			Value:  fmt.Sprintf("```%s```ㅤ", t.Task),
+			Inline: false,
+		})
+	}
+
+	embed := &discordgo.MessageEmbed{
+		Title: "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ",
+		Color: 0xE0C460,
+		Author: &discordgo.MessageEmbedAuthor{
+			Name:    member.User.Username,
+			IconURL: member.User.AvatarURL("100"),
+		},
+		Description: fmt.Sprintf("**Total**\n╠ Uncompleted Tasks: **%d**\n╚ Completed Tasks: **%d**\nㅤ", len(todos), numCompleted),
+		Fields:      tasks,
+	}
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{embed},
+		},
+	})
 }
 
 func getAllTodoResponse(s *discordgo.Session, i *discordgo.InteractionCreate, todos []todo, err error) {
-    if err != nil {
-        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-            Type: discordgo.InteractionResponseChannelMessageWithSource,
-            Data: &discordgo.InteractionResponseData{
-                Content: "Failed to get todos: " + err.Error(),
-            },
-        })
-        return
-    }
+	if err != nil {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to get todos: " + err.Error(),
+			},
+		})
+		return
+	}
 
-    embed := embed.NewEmbed()
-    embed.SetTitle("Todos")
-    embed.SetColor(0xffab40)
-    for _, t := range todos {
-        embed.AddField(fmt.Sprintf("Id: %d", t.Id), fmt.Sprintf("Person: %s\n Task: %s\n Completed: %t", t.User, t.Task, t.Completed))
-    }
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Embeds: []*discordgo.MessageEmbed{embed.MessageEmbed},
-        },
-    })
+	embed := embed.NewEmbed()
+	embed.SetTitle("TODOsㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ")
+	embed.SetColor(0xE0C460)
+	embed.SetThumbnail("https://cdn-icons-png.flaticon.com/512/9717/9717679.png")
+	for _, t := range todos {
+		embed.AddField(fmt.Sprintf("ID: %d", t.Id), fmt.Sprintf("%s\n```%s```ㅤ", t.User, t.Task))
+	}
+	embed.SetFooter(fmt.Sprintf("Total: %d", len(todos)))
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{embed.MessageEmbed},
+		},
+	})
 }
